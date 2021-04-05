27ab9313fbb0f0c976c1c67f3aaa5ddf5311bc28
Taylor <tw10262@gmail.com>
2021-04-05 16:28:25 -0700

    week 1 debugging and  data types

---
+++ AA-week1/dataTypes.js
@@ -0,0 +1,43 @@
+// Number type 
+
+    // exactly what is sounds like, numbers, values that can be minipulated by math operations 
+    // % - modulo (mod) this operator returns the REMAINDER of the given numbers 
+    // % ex 
+    console.log(10 / 6) // prints: 4 
+
+// Boolean type 
+
+    // represented by only 2 things 
+    // true or false 
+            
+            // ! NOT
+
+            // not is represented by ! (bang)
+            // ex of not 
+            console.log(!true); // prints: false 
+            // opposite of the operation it is attached to 
+
+            // an important ! law 
+                // to evaluate something like this: 
+                !(true && false)
+                // we must first distrib the !
+                //THEN we must chage the operator if the operatior is not changes you will not get the correct result 
+                // the computer does this on its own just for refrence that is what is happening 
+                // final equation
+                !true || !false 
+
+            // && AND
+            
+            // represented with &&
+            // both sides of exuation must equal true to return true 
+            //ex 
+            console.log(false && true); // prints: false 
+            console.log(true && true); //prints: true
+
+            // OR || 
+
+            // represented with || 
+            // will return true as long as one side evaluates to true; 
+            //ex
+            console.log(false || false); // prints: false 
+            console.log(true || false); // prints: true 
