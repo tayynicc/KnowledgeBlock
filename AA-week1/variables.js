b2416fe5b49024da9208fb8cae158d4d3cc86968
Taylor <tw10262@gmail.com>
2021-04-05 16:51:20 -0700

    start var page

---
+++ AA-week1/variables.js
@@ -0,0 +1,9 @@
+// Variables are declared with the let key word 
+
+let words = "some words in a sting"
+
+console.log(words); // prints: "some words in a sting"
+
+// you can initilize a empty variable to put things into later 
+
+let thisVar = 
