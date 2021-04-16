3f9d41d60cc8315fc3497987745f0502e202810f
Taylor <tw10262@gmail.com>
2021-04-15 23:03:48 -0700

    rss

---
+++ AA-week2/closures.js
@@ -0,0 +1,1 @@
+// a closure is the ombination of a function and the envirment inwhich it was declared
--- InitalRssPage.js
+++ InitalRssPage.js
@@ -1,1 +1,82 @@
-https://github.com/appacademy/Module-1-Resources/tree/main/week2/d2-pojo
+https://github.com/appacademy/Module-1-Resources/tree/main/week2/d2-po
+
+
+
+scope
+https://youtu.be/hTU1OSbnov8
+
+closures
+https://youtu.be/1JsJx1x35c0
+https://youtu.be/-xqJo5VRP4A
+
+OBJCT AND ARRAY DESTRUCTURING
+https://youtu.be/UgEaJBz3bjY
+https://youtu.be/G4T2ZgJPKbw
+https://youtu.be/-vR3a11Wzt0
+https://youtu.be/_ApRMRGI-6g
+https://youtu.be/fsybVOVcNg0
+
+objects
+https://youtu.be/qs3F-z6ridc
+https://youtu.be/X0ipw1k7ygU
+
+higher Order functions and arrays
+https://youtu.be/NIq3qLaHCIs
+
+has array methods in this
+https://youtu.be/rRgD1yVwIvE
+
+https://youtu.be/R8rmfD9Y5-c
+// Array Methods
+.forEach()
+https://youtu.be/159EAISAxwg
+
+.Reduce()
+https://youtu.be/NiLUGy1Mh4U
+https://youtu.be/-LFjnY1PEDA
+
+.filter()
+https://youtu.be/qmnH5MT_luk
+https://youtu.be/4_iT6EGkQfk
+https://youtu.be/WrB_--bVbbs
+
+.map()
+https://youtu.be/hfYa4ugeyuc
+
+
+spread op ...
+https://youtu.be/pYI-UuZVtHI
+
+ ...rest
+ https://youtu.be/j3bXlt3NOZA
+
+callbacks
+https://youtu.be/qtfi4-8dj9c
+https://youtu.be/pTbSfCT42_M
+
+helper functions
+https://youtu.be/95mIis5M-gU
+https://youtu.be/yjGZwgKEFMU
+
+for...in loop
+https://youtu.be/prl73KRkB34
+https://youtu.be/Kn06785pkJg
+https://youtu.be/c5a4sNshBA4
+
+lotz of loops
+https://youtu.be/Kn06785pkJg
+
+
+let const var
+https://youtu.be/XgSjoHgy3Rk
+https://youtu.be/dzEieWaOJE0
+https://youtu.be/7tGmS2SPxBo
+
+variable hoisting
+https://youtu.be/j-9_15QBW2s
+https://youtu.be/ppMlvGMT2qE
+https://youtu.be/AplVrrwY1TI
+
+
+cool and sweet melon cake
+ https://youtu.be/ysw50GKwEZ0
